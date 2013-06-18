76c43927bf6b335bf65b697536c805622467c3ef
jzeta <holla@jzeta.com>
2013-06-17 17:32:08 -0700

    ex. 51

--- projects/gothonweb/lib/gothonweb.rb
+++ projects/gothonweb/lib/gothonweb.rb
@@ -5,6 +5,15 @@ require "erb"
 #module Gothonweb
   get '/' do
     greeting = "Hello, World!"
-    erb :index, :locals =>{:greeting => greeting}
+    erb :index, :locals => {:greeting => greeting}
+  end
+
+  get '/hello' do
+    erb :hello_form
+  end
+
+  post '/hello' do
+    greeting = "#{params[:greet] || "Hello"}, #{params[:name] || "Nobody"}"
+    erb :index, :locals => {:greeting => greeting}
   end
 #end
